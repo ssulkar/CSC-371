8b1b368f7524c88d297297a6e9c011b88afd921e
ssulkar <ssulkar@calpoly.edu>
2018-02-19 18:26:31 -0800

    toes crossed p2
    toes crossed p2

---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour {
+
+	public float min=2f;
+	public float max=2f;
+	private Rigidbody2D rb2d; 
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+
+		rb2d.freezeRotation = true;
+		min=transform.position.x;
+		max=transform.position.x+12;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position =new Vector2(Mathf.PingPong(Time.time*2,max-min)+min, transform.position.y);
+	}
+}
