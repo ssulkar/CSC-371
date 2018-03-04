b3a0addc16bc21cddfb8b313b260b4d53d674efb
Michael Lozada <mnmlozada@gmail.com>
2018-03-04 14:57:08 -0800

    cutscene/dialogue

---
+++ Assets/Scripts/Dialogue/DialogueHolder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder: MonoBehaviour {
+
+    public string[] dialogue;
+
+    private DialogueManager dMan;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        dMan = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                if (!dMan.dialogueActive)
+                {
+                    dMan.dialogLines = dialogue;
+                    dMan.currentLine = 0;
+                    dMan.ShowDialogue();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dBox;
+    public Text dText;
+
+    public bool dialogueActive;
+
+    public string[] dialogLines;
+    public int currentLine;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentLine = 0;
+        dialogLines = new string[3];
+        fillDialogue();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // every time player presses space, progresses onto next string
+        if (dialogueActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            currentLine++;
+        }
+
+        //once all text from array is read
+        if(currentLine >= dialogLines.Length)
+        {
+            dBox.SetActive(false);
+            dialogueActive = false;
+
+            // reset counter to start in future dialogue array
+            currentLine = 0;
+        }
+
+
+        dText.text = dialogLines[currentLine];
+	}
+
+    // shows text box, called by other scripts
+    public void ShowDialogue()
+    {
+        dialogueActive = true;
+        dBox.SetActive(true);
+
+    }
+
+    public void fillDialogue()
+    {
+        dialogLines[0] = "STRAIGHT UP";
+        dialogLines[1] = "LA FLAME!";
+        dialogLines[2] = "MOM: STOP MAKING LISTENING TO THAT HIPPITY HOP GARBAGE!";
+    }
+}
