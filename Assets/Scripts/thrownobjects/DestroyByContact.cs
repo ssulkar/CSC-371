eb28db949b94708f29a93a026b2836ba3031ee18
cherna74 <cherna74@calpoly.edu>
2018-03-05 18:17:37 -0800

    done boss chasing and object throwing

---
+++ Assets/Scripts/thrownobjects/DestroyByContact.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	//
+	private float nextActionTime = 0.0f;
+	private float period = 5.0f; // 5 seconds
+
+	void Start ()
+	{
+		nextActionTime = Time.time + period;
+	}
+
+	void Update (){
+
+		if (Time.time >= nextActionTime) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+
+		if ( other.gameObject.tag == "Player")
+		{
+			//Destroy (other.gameObject);  if we want to destroy certain things it gets in contact with
+			Destroy (gameObject);
+		}
+			
+		return;
+	}
+}
