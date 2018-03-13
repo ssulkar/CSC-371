1a89dfbd2649eb955735672afa68d74a4664e389
Sam Freed <sfreed141@gmail.com>
2018-03-12 20:44:13 -0700

    add evolve animation and triggers

---
+++ Assets/TriggerEvolution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvolution : MonoBehaviour {
+	public float evolveDuration = 3.0f;
+	private Animator animator;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag("Player")) {
+			animator = other.gameObject.GetComponent<Animator>();
+
+			StartCoroutine(EvolveCoroutine());
+		}
+	}
+
+	IEnumerator EvolveCoroutine() {
+		animator.SetTrigger("evolveTrigger");
+		animator.SetBool("isEvolving", true);
+
+		yield return new WaitForSeconds(evolveDuration);
+
+		animator.SetBool("isEvolving", false);
+	}
+}
